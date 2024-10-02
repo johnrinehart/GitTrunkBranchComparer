e49bd840d8899debfb87d39c8017010285a072d8
John Rinehart <johnrinehart@outlook.com>
2024-10-02 15:12:32 -0300

    fix: add filtered word and format message

--- GitTrunkBranchComparer/Program2.cs
+++ GitTrunkBranchComparer/Program2.cs
@@ -25,16 +25,23 @@ static internal class Program2
         var list2 = commits2.Select(commit => $"{commit.Author.When:yyyy/MM/dd HH:mm:ss} {commit.MessageShort}").ToList();
         var list3 = list1.Except(list2).Order().ToList();
 
-        if (!string.IsNullOrEmpty(filter))
-        {
-            list3 = contains ? list3.Where(x => x.Contains(filter)).ToList() : list3.Where(x => !x.Contains(filter)).ToList();
-        }
-
         // Log program version
         var version = typeof(Program).Assembly.GetName().Version;
         AnsiConsole.WriteLine($"Program Version: {version}\n");
 
-        AnsiConsole.MarkupLine($"Commits since [gold3_1]{after}[/] that are on [gold3_1]{branch1}[/] but not on [gold3_1]{branch2}[/]:\n");
+        var commitsMessage = $"Commits since \t[gold3_1]{after}[/] \n" +
+                $"on \t\t[gold3_1]{branch1}[/] \n" +
+                $"but not on \t[gold3_1]{branch2}[/]";
+
+        if (!string.IsNullOrEmpty(filter))
+        {
+            commitsMessage += contains ? " \nincluding " : " \nexcluding ";
+            commitsMessage += $"\t[gold3_1]{filter}[/]";
+            list3 = contains ? list3.Where(x => x.Contains(filter)).ToList() : list3.Where(x => !x.Contains(filter)).ToList();
+        }
+
+        AnsiConsole.MarkupLine(commitsMessage + ":\n");
+
         foreach (var item in list3)
         {
             var datepart = item[..19];
